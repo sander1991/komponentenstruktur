5058c13f3481fc378f20c9a7941acd061092ce60
sander19911 <sander.buchheim@smail.th-koeln.de>
2022-09-03 01:19:00 +0200

    Smart und Dumb Konzept fertig

--- src/app/01-no-concept/no-concept-toolbar/no-concept-toolbar.component.ts
+++ src/app/01-no-concept/no-concept-toolbar/no-concept-toolbar.component.ts
@@ -1,4 +1,6 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { takeUntil } from 'rxjs';
+import { Unsub } from 'src/app/shared/utils/Unsub';
 import { ComponentEvent } from '../../shared/models/component-event';
 import { CatFactService } from '../../shared/services/cat-fact.service';
 
@@ -7,14 +9,19 @@ import { CatFactService } from '../../shared/services/cat-fact.service';
   templateUrl: './no-concept-toolbar.component.html',
   styleUrls: ['./no-concept-toolbar.component.css'],
 })
-export class NoConceptToolbarComponent {
+export class NoConceptToolbarComponent extends Unsub {
   @Output() event = new EventEmitter<ComponentEvent>();
 
-  constructor(private catFactService: CatFactService) {}
+  constructor(private catFactService: CatFactService) {
+    super();
+  }
 
   public request(length: string) {
-    this.catFactService.getFact(length).subscribe((fact) => {
-      this.event.emit({ type: 'EVENT_ADD', data: fact });
-    });
+    this.catFactService
+      .getFact(length)
+      .pipe(takeUntil(this.unsubscribe$))
+      .subscribe((fact) => {
+        this.event.emit({ type: 'EVENT_ADD', data: fact });
+      });
   }
 }
--- src/app/02-smart-dumb-concept/smart-dumb-concept.component.ts
+++ src/app/02-smart-dumb-concept/smart-dumb-concept.component.ts
@@ -1,26 +1,30 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
-import { BehaviorSubject, Observable } from 'rxjs';
+import { Component } from '@angular/core';
+import { BehaviorSubject, Observable, takeUntil, tap } from 'rxjs';
 import { CatFact } from '../shared/models/cat-fact';
 import { ComponentEvent } from '../shared/models/component-event';
 import { CatFactService } from '../shared/services/cat-fact.service';
+import { Unsub } from '../shared/utils/Unsub';
 
 @Component({
   selector: 'app-smart-dumb-concept',
   templateUrl: './smart-dumb-concept.component.html',
   styleUrls: ['./smart-dumb-concept.component.css'],
 })
-export class SmartDumbConceptComponent {
+export class SmartDumbConceptComponent extends Unsub {
   catFacts: BehaviorSubject<CatFact[]> = new BehaviorSubject<CatFact[]>([]);
-  catFacts$: Observable<any> = this.catFacts.asObservable();
+  catFacts$: Observable<CatFact[]> = this.catFacts.asObservable();
 
-  @ViewChild('refreshButton') refreshBtn!: ElementRef;
-
-  constructor(private catFactService: CatFactService) {}
+  constructor(private catFactService: CatFactService) {
+    super();
+  }
 
   public onEvent(event: ComponentEvent) {
     switch (event.type) {
       case 'EVENT_ADD':
         this.addCatFact(event.data);
+        break;
+      case 'EVENT_DELETE':
+        this.removeCatFact(event.data);
         break;
       default:
         console.error(`unknown event fetched: ${JSON.stringify(event)}`);
@@ -29,9 +33,19 @@ export class SmartDumbConceptComponent {
   }
 
   private addCatFact(length: string) {
-    // this.catFactService.getFact(length).pipe(tap((fact) => {
-    //   const newArr = [this.catFacts.value, fact];
-    //   this.catFacts.next(newArr);
-    // })).subscribe();
+    this.catFactService
+      .getFact(length)
+      .pipe(
+        takeUntil(this.unsubscribe$),
+        tap((fact) => {
+          const newArr = [...this.catFacts.value, fact];
+          this.catFacts.next(newArr);
+        })
+      )
+      .subscribe();
+  }
+
+  private removeCatFact(index: number) {
+    this.catFacts.value.splice(index, 1);
   }
 }
