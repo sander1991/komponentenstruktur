e8d2c6d9ef4d8a679ccf45a2fbb4d1174df8eb08
Sander Buchheim <sander.buchheim@opitz-consulting.com>
2022-09-07 12:47:53 +0200

    Standalone Component

--- src/app/02-feature-module-concept/feature-details-standalone/feature-details-standalone.component.ts
+++ src/app/02-feature-module-concept/feature-details-standalone/feature-details-standalone.component.ts
@@ -1,14 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { ActivatedRoute } from '@angular/router';
 import { combineLatest, Observable, of, switchMap } from 'rxjs';
 import { CatFact } from 'src/app/shared/models/cat-fact';
 
 @Component({
-  selector: 'app-feature-details',
-  templateUrl: './feature-details.component.html',
-  styleUrls: ['./feature-details.component.css'],
+  selector: 'app-feature-details-standalone',
+  templateUrl: './feature-details-standalone.component.html',
+  styleUrls: ['./feature-details-standalone.component.css'],
+  standalone: true,
+  imports: [MatFormFieldModule, MatInputModule, FormsModule, CommonModule],
 })
-export class FeatureDetailsComponent {
+export class FeatureDetailsStandaloneComponent {
   catFact$: Observable<CatFact> = combineLatest([
     this.activatedRoute.params,
     this.activatedRoute.queryParams,
--- src/app/02-feature-module-concept/feature-module-concept.module.ts
+++ src/app/02-feature-module-concept/feature-module-concept.module.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SharedModule } from './../shared/shared.module';
+import { FeatureDetailsStandaloneComponent } from './feature-details-standalone/feature-details-standalone.component';
 import { FeatureDetailsComponent } from './feature-details/feature-details.component';
 import { FeatureFactsContentComponent } from './feature-facts/feature-facts-content/feature-facts-content.component';
 import { FeatureFactsToolbarComponent } from './feature-facts/feature-facts-toolbar/feature-facts-toolbar.component';
@@ -24,10 +25,23 @@ const routes: Routes = [
     title: 'Feature Module - Detail',
     component: FeatureDetailsComponent,
   },
+  // {
+  //   path: 'facts/:id',
+  //   title: 'Feature Module - Detail STANDALONE',
+  //   loadComponent: () =>
+  //     import(
+  //       './feature-details-standalone/feature-details-standalone.component'
+  //     ).then((m) => m.FeatureDetailsStandaloneComponent),
+  // },
 ];
 
 @NgModule({
-  imports: [CommonModule, SharedModule, RouterModule.forChild(routes)],
+  imports: [
+    CommonModule,
+    SharedModule,
+    RouterModule.forChild(routes),
+    FeatureDetailsStandaloneComponent,
+  ],
   declarations: [
     FeatureModuleConceptComponent,
     FeatureDetailsComponent,
